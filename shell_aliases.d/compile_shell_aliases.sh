b83dc75b985159da154dc941010859e52936cc3d
Marcin Nowicki <pr0d1r2@gmail.com>
2017-07-12 07:15:21 +0200

    Return to directory even if error occured

--- shell_aliases.d/compile_shell_aliases.sh
+++ shell_aliases.d/compile_shell_aliases.sh
@@ -1,7 +1,13 @@
 function compile_shell_aliases() {
   local compile_shell_aliases_OLD_PWD=`pwd -P`
+  local compile_shell_aliases_ERROR
   cd $PROJECTS_PATH/shell_aliases_compiler || return $?
-  sh setup.sh $@ || return $?
-  source ~/.compiled_shell_aliases.sh
+  sh setup.sh $@
+  compile_shell_aliases_ERROR=$?
+  if [ $compile_shell_aliases_ERROR -eq 0 ]; then
+    source ~/.compiled_shell_aliases.sh
+    compile_shell_aliases_ERROR=$?
+  fi
   cd $compile_shell_aliases_OLD_PWD
+  return $compile_shell_aliases_ERROR
 }
