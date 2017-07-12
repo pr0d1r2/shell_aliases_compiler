1666d2b91e53bb49f8fca9eccdee38a089dcab99
Marcin Nowicki <pr0d1r2@gmail.com>
2017-07-12 07:11:42 +0200

    Use unified projects path

--- shell_aliases.d/compile_shell_aliases.sh
+++ shell_aliases.d/compile_shell_aliases.sh
@@ -1,6 +1,6 @@
 function compile_shell_aliases() {
   local compile_shell_aliases_OLD_PWD=`pwd -P`
-  cd ~/projects/shell_aliases_compiler || return $?
+  cd $PROJECTS_PATH/shell_aliases_compiler || return $?
   sh setup.sh $@ || return $?
   source ~/.compiled_shell_aliases.sh
   cd $compile_shell_aliases_OLD_PWD
