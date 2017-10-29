48c92159eccd2ee52336d39faaf008cf31e0e28d
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-29 07:21:10 +0100

    Ensure to add git stuff at top level of repo

--- setup.sh
+++ setup.sh
@@ -227,7 +227,7 @@ parallel \
   "source $HOME/projects/ubercommit/shell_aliases.d/UBERCOMMIT_PATH.sh && \
    source $HOME/projects/ubercommit/shell_aliases.d/ubercommit_add.sh && \
    source $HOME/projects/ubercommit/shell_aliases.d/ubercommit_add_file_suffix.sh && \
-   cd {} &&\
+   cd {} && cd \$(git rev-parse --show-toplevel) && \
    SKIP_EXAMPLES=1 ubercommit_add && git status -sb | grep -q \"^?? \" && \
    source $HOME/projects/ubercommit/shell_aliases.d/ubercommit_add_file_suffix_examples.sh && \
    ubercommit_add_file_suffix_examples sh" \
