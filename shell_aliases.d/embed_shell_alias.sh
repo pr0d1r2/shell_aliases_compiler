66f8f62cd043930806f94949130fa5eb2178acfc
Marcin Nowicki <pr0d1r2@gmail.com>
2017-10-29 04:50:47 +0100

    Use better parallelism. Conform to ShellCheck

--- shell_aliases.d/embed_shell_alias.sh
+++ shell_aliases.d/embed_shell_alias.sh
@@ -1,13 +1,12 @@
 function embed_shell_alias() {
   local embed_shell_alias_NAME
   local embed_shell_alias_BASE
-  local embed_shell_alias_ATOM_LOCATION
-  for embed_shell_alias_NAME in $@
+  for embed_shell_alias_NAME in "$@"
   do
     local embed_shell_alias_FILE="$embed_shell_alias_NAME.sh"
     echo
     echo "Embedding '$embed_shell_alias_NAME' into '$embed_shell_alias_FILE'"
-    embed_shell_alias_BASE=`shell_alias_location $embed_shell_alias_NAME`
+    embed_shell_alias_BASE=$(shell_alias_location "$embed_shell_alias_NAME")
     case $embed_shell_alias_BASE in
       "")
         echo "There is no '$embed_shell_alias_NAME' alias. Aborting!"
@@ -17,15 +16,15 @@ function embed_shell_alias() {
 
     echo "Adding:"
 
-    for embed_shell_alias_ATOM_LOCATION in `shell_alias_location \`shell_alias_dependencies_recursive $embed_shell_alias_NAME | sort | uniq\` | sort | uniq`
-    do
-      echo $embed_shell_alias_ATOM_LOCATION
-      cat $embed_shell_alias_ATOM_LOCATION >> $embed_shell_alias_FILE
-    done
+    shell_alias_dependencies_recursive "$embed_shell_alias_NAME" | sort -u | \
+      parallel \
+        "source $SHELL_ALIASES_COMPILER_ALIASES_PATH/shell_alias_location.sh && \
+         shell_alias_location {}" | sort -u | \
+           parallel "cat {} ; echo {} 1>&2" >> "$embed_shell_alias_FILE"
 
-    echo $embed_shell_alias_BASE
-    cat $embed_shell_alias_BASE >> $embed_shell_alias_FILE
+    echo "$embed_shell_alias_BASE"
+    cat "$embed_shell_alias_BASE" >> "$embed_shell_alias_FILE"
 
-    echo "$embed_shell_alias_NAME \$@ || exit \$?" >> $embed_shell_alias_FILE
+    echo "$embed_shell_alias_NAME \$@ || exit \$?" >> "$embed_shell_alias_FILE"
   done
 }
