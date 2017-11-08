2275b00db852fe487d3c478fc1890a92e9618f37
Marcin Nowicki <pr0d1r2@gmail.com>
2017-11-08 05:18:42 +0100

    Update hook

--- .git-hooks/pre_commit/ensure_no_sort_pipe_uniq.rb
+++ .git-hooks/pre_commit/ensure_no_sort_pipe_uniq.rb
@@ -4,28 +4,33 @@
 #   enabled: true
 #   include:
 #     - '**/*.sh'
-module Overcommit::Hook::PreCommit
-  class EnsureNoSortPipeUniq < Base
-    def run
-      errors = []
+module Overcommit
+  module Hook
+    module PreCommit
+      # Prevent 'sort | uniq' from happening as 'sort -u' is faster
+      class EnsureNoSortPipeUniq < Base
+        def run
+          errors = []
 
-      check_files.each do |file|
-        match = File.read(file).match(/sort\s{0,}\|\s{0,}uniq/)
-        if match
-          errors << "#{file}: contains '#{match[0]}' (can be replaced with 'sort -u')"
+          check_files.each do |file|
+            match = File.read(file).match(/sort\s{0,}\|\s{0,}uniq/)
+            if match
+              errors << "#{file}: contains '#{match[0]}' (can be replaced with 'sort -u')"
+            end
+          end
+
+          return :fail, errors.join("\n") if errors.any?
+
+          :pass
         end
-      end
 
-      return :fail, errors.join("\n") if errors.any?
+        private
 
-      :pass
-    end
-
-    private
-
-    def check_files
-      applicable_files.reject do |file|
-        File.basename(file) =~ /^ensure_no_sort_pipe_uniq\.rb$/
+        def check_files
+          applicable_files.reject do |file|
+            File.basename(file) =~ /^ensure_no_sort_pipe_uniq\.rb$/
+          end
+        end
       end
     end
   end
